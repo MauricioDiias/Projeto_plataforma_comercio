ecd7a94aff26e253dab78c6ba5e40811f2e10cb7
mauricio.abreu <mauricio.abreu@botdesigner.io>
2023-10-10 16:23:43 -0300

    Aualizando top bar

--- FrontEnd/plataforma_comercio/src/shared/components/TopBar/index.jsx
+++ FrontEnd/plataforma_comercio/src/shared/components/TopBar/index.jsx
@@ -1,7 +1,20 @@
 function TopBar() {
+  
   return (
-    <div style={{ marginLeft:'250px', height: "91px", backgroundColor: "orange" }}>
-      <h1>topheader</h1>
+    <div
+      style={{
+        marginLeft: "250px",
+        height: "91px",
+        border: "1px solid #dee2e6",
+        padding: "25px",
+        display: "flex",
+        alignItems: "center",
+        fontFamily: "Helvetica",
+        gap:'50px'
+      }}
+    >
+      <i className="pi pi-bars"></i>
+      <h3 style={{color:'#495057'}}>Home / Cadastrar Produto</h3>
     </div>
   );
 }
