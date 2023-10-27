e6863f90191de57f6129b43a1bd84cdaf4401e82
MauricioDiias <mauriciod664@gmail.com>
2023-10-27 00:52:45 -0300

    adiciona login card

--- FrontEnd/plataforma_comercio/src/pages/Login/index.jsx
+++ FrontEnd/plataforma_comercio/src/pages/Login/index.jsx
@@ -1,7 +1,14 @@
+
+import { Card } from 'primereact/card';
+        
 function Login(){
 return(
     <>
     login
+<div style={{display:'flex',justifyContent:'center'}}>
+
+    <Card title={'Login'} style={{width:'500px',marginTop:'200px'}}/>
+</div>
     </>
 )
 }
