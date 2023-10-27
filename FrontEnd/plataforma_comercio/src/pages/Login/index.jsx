c3ac7ce7675c839f98e5fb04066868fdcbaf4ae4
MauricioDiias <mauriciod664@gmail.com>
2023-10-27 03:28:21 -0300

    ddfd

--- FrontEnd/plataforma_comercio/src/pages/Login/index.jsx
+++ FrontEnd/plataforma_comercio/src/pages/Login/index.jsx
@@ -1,15 +1,16 @@
+import { Card } from "primereact/card";
 
-import { Card } from 'primereact/card';
-        
-function Login(){
-return(
+function Login() {
+  return (
     <>
-    login
-<div style={{display:'flex',justifyContent:'center'}}>
-
-    <Card title={'Login'} style={{width:'500px',marginTop:'200px'}}/>
-</div>
+      login
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Card
+          title={"Login"}
+          style={{ width: "500px", marginTop: "200px" }}
+        ></Card>
+      </div>
     </>
-)
+  );
 }
-export default Login
+export default Login;
