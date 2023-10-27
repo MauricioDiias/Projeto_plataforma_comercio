754d9a608aa04fc2db4be519c39a0de5e2a5d719
mauricio.abreu <mauricio.abreu@botdesigner.io>
2023-10-27 17:43:18 -0300

    tela de login

--- FrontEnd/plataforma_comercio/src/pages/Login/index.jsx
+++ FrontEnd/plataforma_comercio/src/pages/Login/index.jsx
@@ -1,16 +1,46 @@
 import { Card } from "primereact/card";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import './styles.css'
 
 function Login() {
   return (
-    <>
-      login
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Card
-          title={"Login"}
-          style={{ width: "500px", marginTop: "200px" }}
-        ></Card>
-      </div>
-    </>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        height:'100vh'
+      }}
+      className="container"
+    >
+      <Card
+        title={"Login"}
+        style={{ width: "500px", margin: "200px 0 300px 0" }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div className="p-field">
+            <span className="p-input-icon-left">
+              <i className="pi pi-search" />
+              <InputText placeholder="Email" />
+            </span>
+          </div>
+          <div className="p-field">
+            <span className="p-input-icon-left">
+              <i className="pi pi-search" />
+              <InputText type="password" placeholder="Senha" />
+            </span>
+          </div>
+          <Button label="Log In" type="submit" />
+        </div>
+      </Card>
+    </div>
   );
 }
 export default Login;
