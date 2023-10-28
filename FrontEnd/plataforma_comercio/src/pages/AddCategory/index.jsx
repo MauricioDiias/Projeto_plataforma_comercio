9af20ba1c2e40e4b8fbcd8c6d7d20d8c0328e3ad
MauricioDiias <mauriciod664@gmail.com>
2023-10-28 15:25:06 -0300

    categoria

--- FrontEnd/plataforma_comercio/src/pages/AddCategory/index.jsx
+++ FrontEnd/plataforma_comercio/src/pages/AddCategory/index.jsx
@@ -1,5 +1,13 @@
-export const AddCategory=()=>{
-    return(
-        <div>category</div>
-    )
-}
+import { Card } from "primereact/card";
+
+export const AddCategory = () => {
+  return (
+    <div>
+      category
+      <Card title={'Adicionar Categoria'} style={{margin:"40px"}}>
+
+
+      </Card>
+    </div>
+  );
+};
